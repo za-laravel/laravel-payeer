f487a0ac9845a7eea1d73085dc42b6319ef5b574
Lyovkin <anatoliymolinari@gmail.com>
2015-07-29 10:50:18 +0300

    first commit

---
+++ src/ZaLaravel/LaravelPayeer/LaravelPayeerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZaLaravel\LaravelPayeer;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPayeerServiceProvider extends ServiceProvider{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
